1cfe634527c5378c1121afd0dda6f6e587d85783
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-10-10 16:58:27 +0200

    neue Graph in TalsperreDetail eingefügt.

--- vaadin-demo-main/src/main/java/de/ostfalia/application/views/talsperren/TalsperreDetail.java
+++ vaadin-demo-main/src/main/java/de/ostfalia/application/views/talsperren/TalsperreDetail.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.charts.Chart;
 import com.vaadin.flow.component.charts.model.ChartType;
 import com.vaadin.flow.component.charts.model.Configuration;
 import com.vaadin.flow.component.charts.model.DataProviderSeries;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.BeforeEvent;
@@ -36,11 +37,72 @@ public class TalsperreDetail extends BasicLayout implements HasUrlParameter<Long
     public void setParameter(BeforeEvent event, Long parameter) {
         talsperre = service.getTalsperreById(parameter);
 
-        setContent(soChart(talsperre.getId()));
+
+        VerticalLayout layout = new VerticalLayout();
+
+
+        layout.add(soChart(talsperre.getId()));
+        layout.add(soChart21Days(talsperre.getId()));
+
+
+        setContent(layout);
+
         this.getTitle().setText("Detailseite");
 
 
     }
+    public SOChart soChart21Days(Long id) {
+
+        SOChart soChart = new SOChart();
+        soChart.setSize("100%", "500px");
+        String title = "Talsperre " + (talsperre == null ? "" : talsperre.getName()) + " - Letzte 21 Tage";
+        soChart.add(new Title(title));
+
+        // Daten für die letzten 21 Tage abrufen
+        List<Talsperrendaten> data = service.getDatenByIDLast21Days(id);
+
+        TimeData xValues = new TimeData();
+        Data abgabe = new Data();
+        Data zufluss = new Data(), inhalt = new Data();
+
+        for (int x = 0; x < data.size(); x++) {
+            zufluss.add(data.get(x).getZufluss());
+            inhalt.add(data.get(x).getStauinhalt());
+            abgabe.add(data.get(x).getAbgabe());
+            xValues.add(data.get(x).getTstamp());
+        }
+
+        // Line chart mit x und y werten füllen
+        LineChart lineAbgabe = new LineChart(xValues, abgabe);
+        lineAbgabe.setName("Wasserabgabe");
+        LineChart lineZufluss = new LineChart(xValues, zufluss);
+        lineZufluss.setName("Zufluss");
+        LineChart lineInhalt = new LineChart(xValues, inhalt);
+        lineInhalt.setName("Inhalt");
+
+        // x Achse für das Frontend
+        XAxis xAxis = new XAxis(DataType.DATE);
+        xAxis.setDivisions(21); // Daten für 21 Tage
+        xAxis.setName("Datum");
+        // y Achse im Frontend
+        YAxis yAxis = new YAxis(DataType.NUMBER);
+        yAxis.setName("Wassermenge");
+        // Rechtwinklige Achsen
+        RectangularCoordinate rc = new RectangularCoordinate(xAxis, yAxis);
+        // Line chart im rechtwinkligen Koordinatensystem zeichnen
+        lineAbgabe.plotOn(rc);
+        lineZufluss.plotOn(rc);
+        lineInhalt.plotOn(rc);
+
+        // Line charts der umfassenden Eltern Komponente hinzufügen
+        soChart.add(lineAbgabe);
+        soChart.add(lineZufluss);
+        soChart.add(lineInhalt);
+
+        return soChart;
+    }
+
+
     public SOChart soChart(Long id){
 
         SOChart soChart = new SOChart();
