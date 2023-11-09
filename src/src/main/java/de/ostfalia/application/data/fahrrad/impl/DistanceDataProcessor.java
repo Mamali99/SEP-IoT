0f07f32adbaabb0b5bd6d622f1331725e4e50b1a
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-11-09 19:22:31 +0100

    .

--- src/src/main/java/de/ostfalia/application/data/fahrrad/impl/DistanceDataProcessor.java
+++ src/src/main/java/de/ostfalia/application/data/fahrrad/impl/DistanceDataProcessor.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -41,11 +42,11 @@ public class DistanceDataProcessor extends AbstractDataProcessor {
         return bikeService.findLastActivityByChannel(channel);
     }
 
+    /*
     @Override
     protected List<ProcessedData> calculateData(List<Bicycle> bicycles) {
         List<ProcessedData> distanceData = new ArrayList<>();
         for (Bicycle bike : bicycles) {
-
             BigDecimal realRotationsPerSecond = bike.getRotations().divide(new BigDecimal(4), 2, RoundingMode.HALF_UP);
             BigDecimal circumference = new BigDecimal("2.111"); // Radumfang in Metern
             BigDecimal distance = realRotationsPerSecond.multiply(circumference);
@@ -56,6 +57,50 @@ public class DistanceDataProcessor extends AbstractDataProcessor {
             System.out.println("Strecke: " + p.getValue());
         }
         return distanceData;
+    }
+
+     */
+    @Override
+    protected List<ProcessedData> calculateData(List<Bicycle> bicycles) {
+        // Sortieren der Fahrraddaten nach Zeitstempel
+        bicycles.sort((b1, b2) -> b1.getTime().compareTo(b2.getTime()));
+
+        // Initialisierung des ersten Intervalls
+        LocalDateTime intervalStart = bicycles.get(0).getTime();
+        BigDecimal intervalDistance = BigDecimal.ZERO;
+        List<ProcessedData> intervalDataList = new ArrayList<>();
+
+        // Bestimmen der Intervallgröße, z.B. 5 Minuten
+        Duration intervalSize = Duration.ofMinutes(30);
+
+        // Durchlaufen der Fahrraddaten und Aggregieren der Distanzen in Intervallen
+        for (Bicycle bike : bicycles) {
+            // Überprüfen, ob das aktuelle Fahrradobjekt zum aktuellen Intervall gehört
+            if (bike.getTime().isBefore(intervalStart.plus(intervalSize))) {
+                // Aggregieren Sie die Distanz für dieses Intervall
+                BigDecimal realRotationsPerSecond = bike.getRotations().divide(new BigDecimal(4), 2, RoundingMode.HALF_UP);
+                BigDecimal circumference = new BigDecimal("2.111"); // Radumfang in Metern
+                BigDecimal distance = realRotationsPerSecond.multiply(circumference).multiply(new BigDecimal("60")); // Distanz pro Minute
+                intervalDistance = intervalDistance.add(distance);
+            } else {
+                // Speichern der aggregierten Daten für das aktuelle Intervall
+                intervalDataList.add(new ProcessedData(bike.getChannel(), intervalDistance, intervalStart));
+
+                // Vorbereitung des nächsten Intervalls
+                intervalStart = bike.getTime(); // oder intervalStart.plus(intervalSize) für lückenlose Intervalle
+                intervalDistance = BigDecimal.ZERO;
+            }
+        }
+
+        // Stellen Sie sicher, dass Sie die Daten für das letzte Intervall nicht verlieren
+        if (!intervalDistance.equals(BigDecimal.ZERO)) {
+            intervalDataList.add(new ProcessedData(bicycles.get(bicycles.size() - 1).getChannel(), intervalDistance, intervalStart));
+        }
+        for(ProcessedData p: intervalDataList){
+            System.out.println("Strecke: " + p.getValue());
+        }
+
+        return intervalDataList;
     }
 
 
