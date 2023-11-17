a015383eca165f07a9f3ceaa389324d905026506
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-11-17 18:41:19 +0100

    die Änderung von developS2-Hana wurde gemergt. Die berechnete Daten für Distance machen kein Sinn für nur 1 Minuten. Es gibt einen Berechnungsfehler in calculateData in DistanceDataProcessor-Klasse

--- src/src/main/java/de/ostfalia/application/data/fahrrad/impl/DistanceDataProcessor.java
+++ src/src/main/java/de/ostfalia/application/data/fahrrad/impl/DistanceDataProcessor.java
@@ -20,6 +20,9 @@ public class DistanceDataProcessor extends AbstractDataProcessor {
 
     private String processorName = "Distance";
 
+    LocalDateTime start;
+    LocalDateTime end;
+
     @Autowired
     public DistanceDataProcessor(BikeService bikeService) {
         this.bikeService = bikeService;
@@ -27,7 +30,15 @@ public class DistanceDataProcessor extends AbstractDataProcessor {
 
     @Override
     protected List<Bicycle> fetchData(int channel, LocalDateTime startTime, LocalDateTime endTime) {
-        return bikeService.getDataWithTimeSpan(channel, startTime, endTime);
+        this.start = startTime;
+        this.end = endTime;
+        List<Bicycle> bicycles = bikeService.getDataWithTimeSpan(channel, startTime, endTime);
+
+        for (Bicycle b: bicycles
+             ) {
+            System.out.println("Channel: " + b.getChannel() + " Time: " + b.getTime() + " Rotations: " + b.getRotations());
+        }
+        return bicycles;
     }
 
     // Implementierung für die Abfrage von Daten seit einem bestimmten Zeitpunkt
@@ -43,6 +54,7 @@ public class DistanceDataProcessor extends AbstractDataProcessor {
     }
 
 
+
     @Override
     protected List<ProcessedData> calculateData(List<Bicycle> bicycles, int intervalInMinutes) {
 
@@ -54,6 +66,14 @@ public class DistanceDataProcessor extends AbstractDataProcessor {
                 BigDecimal distance = realRotationsPerSecond.multiply(circumference);
                 distanceData.add(new ProcessedData(bike.getChannel(), distance, bike.getTime(), processorName));
             }
+            /*
+            for(ProcessedData p: distanceData){
+                System.out.println(p.getValue());
+            }
+
+             */
+
+            System.out.println("---------------- neue Distanz================>" + calculateDistanceForInterval(bicycles, start, end));
             return distanceData;
 
         }
@@ -101,6 +121,64 @@ public class DistanceDataProcessor extends AbstractDataProcessor {
 
         return intervalDataList;
     }
+
+    /*
+    public BigDecimal calculateDistanceForInterval(List<Bicycle> bicycleList, LocalDateTime startTime, LocalDateTime endTime) {
+        System.out.println(startTime.toString());
+        System.out.println(endTime.toString());
+        BigDecimal totalRotations = BigDecimal.ZERO; // Summe aller Rotationen
+        BigDecimal circumference = new BigDecimal("2.111"); // Radumfang in Metern
+        BigDecimal totalDistance; // Gesamtdistanz
+        int secondsInInterval = 0; // Sekunden im Intervall
+
+        // Summieren Sie alle Rotationswerte innerhalb des Zeitintervalls.
+        for (Bicycle bike : bicycleList) {
+            if (!bike.getTime().isBefore(startTime) && !bike.getTime().isAfter(endTime)) {
+                totalRotations = totalRotations.add(bike.getRotations());
+                secondsInInterval++; // Zählen Sie die Anzahl der Datenpunkte
+            }
+        }
+
+        // Berechnen Sie die durchschnittliche Anzahl der Umdrehungen pro Sekunde.
+        if (secondsInInterval > 0) {
+            BigDecimal averageRotationsPerSecond = totalRotations.divide(new BigDecimal(secondsInInterval), 2, RoundingMode.HALF_UP);
+            averageRotationsPerSecond = averageRotationsPerSecond.divide(new BigDecimal(4), 2, RoundingMode.HALF_UP); // Durchschnittliche Rotationen pro Sekunde
+            // Berechnen Sie die Gesamtdistanz für das Intervall.
+            totalDistance = averageRotationsPerSecond.multiply(circumference).multiply(new BigDecimal(secondsInInterval)); // Gesamtdistanz
+        } else {
+            totalDistance = BigDecimal.ZERO;
+        }
+
+        // Debug: Ausgabe der Gesamtdistanz
+        System.out.println("Total distance for interval from " + startTime + " to " + endTime + ": " + totalDistance);
+
+        return totalDistance.setScale(2, RoundingMode.HALF_UP); // Runden Sie die Gesamtdistanz auf zwei Dezimalstellen
+    }
+
+     */
+
+    public BigDecimal calculateDistanceForInterval(List<Bicycle> bicycleList, LocalDateTime startTime, LocalDateTime endTime) {
+        BigDecimal totalDistance = BigDecimal.ZERO; // Gesamtdistanz
+        BigDecimal circumference = new BigDecimal("2.111"); // Radumfang in Metern
+
+        // Berechnen Sie die Gesamtdistanz innerhalb des Zeitintervalls.
+        for (Bicycle bike : bicycleList) {
+            if (!bike.getTime().isBefore(startTime) && !bike.getTime().isAfter(endTime)) {
+                // Teilen Sie die gemessene Rotationsfrequenz durch 4, um die tatsächlichen Umdrehungen pro Sekunde zu erhalten.
+                BigDecimal rotationsPerSecond = bike.getRotations().divide(new BigDecimal(4), 2, RoundingMode.HALF_UP);
+                // Berechnen Sie die Distanz pro Sekunde und addieren Sie sie zur Gesamtdistanz.
+                BigDecimal distancePerSecond = rotationsPerSecond.multiply(circumference);
+                totalDistance = totalDistance.add(distancePerSecond);
+            }
+        }
+
+        // Debug: Ausgabe der Gesamtdistanz
+        System.out.println("Total distance for interval from " + startTime + " to " + endTime + ": " + totalDistance);
+
+        // Da die Distanz pro Sekunde berechnet wurde, multiplizieren Sie nicht mit der Anzahl der Sekunden im Intervall.
+        return totalDistance.setScale(2, RoundingMode.HALF_UP); // Runden Sie die Gesamtdistanz auf zwei Dezimalstellen
+    }
+
 
 
 }
