c5069e4054d1b70b22ae56aefdd3f20ea956299e
Hana Bukvic <72923161+momongi@users.noreply.github.com>
2023-12-10 16:48:25 +0100

    Dynamisch Button Add Test - Neues Layout

--- src/src/main/java/de/ostfalia/application/views/lampen/LampeView.java
+++ src/src/main/java/de/ostfalia/application/views/lampen/LampeView.java
@@ -2,6 +2,7 @@ package de.ostfalia.application.views.lampen;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -15,6 +16,7 @@ import org.vaadin.addons.tatu.ColorPicker;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,12 +27,20 @@ public class LampeView extends BasicLayout {
     private ColorPicker colorPicker;
     private ComboBox<String> commandHistoryDropdown;
 
+    private List<Button> possibleButtons = new ArrayList<>();
+    private HorizontalLayout buttonLayout = new HorizontalLayout();
+    private VerticalLayout rightLayout = new VerticalLayout();
+    private VerticalLayout initialButtonLayout = new VerticalLayout();
+    private VerticalLayout newButtonLayout = new VerticalLayout();
+    private Dialog buttonDialog = new Dialog();
+
     public LampeView(RemoteController remoteController) throws IOException {
         this.remoteController = remoteController;
         setupLayout();
     }
 
     private void setupLayout() {
+
         // Initialisiere die Buttons und ColorPicker
         Button turnOnButton = createButton("Turn On", VaadinIcon.POWER_OFF);
         turnOnButton.addClickListener(e -> executeCommand(new TurnOnCommand(new Java2NodeRedLampAdapter())));
@@ -47,6 +57,18 @@ public class LampeView extends BasicLayout {
         Button partyModeButton = createButton("Party Mode", VaadinIcon.MUSIC);
         partyModeButton.addClickListener(e -> activatePartyMode());
 
+        // Füge die initialen Buttons zum Layout hinzu
+        initialButtonLayout.add(turnOnButton, turnOffButton, blinkButton, delayedCommandButton, partyModeButton);
+
+        // Initialisiere die zusätzlichen Buttons
+        possibleButtons.add(createButton("Button 1", VaadinIcon.ABACUS));
+        possibleButtons.add(createButton("Button 2", VaadinIcon.ACADEMY_CAP));
+
+        // Erstelle den "Plus"-Button
+        Button plusButton = new Button("Plus", e -> openButtonDialog());
+        plusButton.setIcon(VaadinIcon.PLUS.create());
+        newButtonLayout.add(plusButton);
+
         colorPicker = new ColorPicker();
         colorPicker.setLabel("Farbe wählen");
         colorPicker.addValueChangeListener(e -> executeCommand(new SetColorCommand(new Java2NodeRedLampAdapter(), hex2Rgb(e.getValue()))));
@@ -56,27 +78,50 @@ public class LampeView extends BasicLayout {
         updateCommandHistoryDropdown();
 
         // Layout
-        HorizontalLayout buttonLayout1 = new HorizontalLayout(turnOnButton, turnOffButton);
-        HorizontalLayout buttonLayout2 = new HorizontalLayout(blinkButton, delayedCommandButton);
-        HorizontalLayout buttonLayout3 = new HorizontalLayout(partyModeButton, colorPicker);
-
-        VerticalLayout layout = new VerticalLayout(buttonLayout1, buttonLayout2, buttonLayout3, commandHistoryDropdown);
-        this.setContent(layout);
+        initialButtonLayout.getStyle().set("margin", "2px");
+        newButtonLayout.getStyle().set("margin", "2px");
+        initialButtonLayout.getStyle().set("padding", "0px");
+        newButtonLayout.getStyle().set("padding", "0px");
+        buttonLayout.add(initialButtonLayout, newButtonLayout);
+        rightLayout.add(colorPicker, commandHistoryDropdown);
+        HorizontalLayout mainLayout = new HorizontalLayout(buttonLayout, rightLayout);
+        this.setContent(mainLayout);
     }
 
-    // Buttons Generieren
     private Button createButton(String text, VaadinIcon icon) {
         Button button = new Button(text);
         button.setIcon(icon.create());
         button.getStyle().set("width", "100px");
         button.getStyle().set("height", "100px");
         button.getStyle().set("position", "relative");
+        button.getStyle().set("margin", "0px"); // Setzt den Außenabstand auf 0
+        button.getStyle().set("padding", "0px"); // Setzt den Innenabstand auf 0
         button.getIcon().getStyle().set("position", "absolute");
         button.getIcon().getStyle().set("bottom", "10px");
         button.getIcon().getStyle().set("left", "50%");
         button.getIcon().getStyle().set("transform", "translateX(-50%)");
         return button;
     }
+
+    private void addButton(Button button) {
+        newButtonLayout.addComponentAtIndex(newButtonLayout.getComponentCount() - 1, button);
+    }
+
+    private void openButtonDialog() {
+        buttonDialog.removeAll();
+
+        for (Button button : possibleButtons) {
+            button.addClickListener(e -> {
+                addButton(button);
+                possibleButtons.remove(button);
+                buttonDialog.close();
+            });
+            buttonDialog.add(button);
+        }
+
+        buttonDialog.open();
+    }
+
     // vom Event Ausgelöst
 
     private void executeCommand(Command command) {
