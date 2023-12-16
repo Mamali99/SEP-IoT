5adaa006da7e6ec325119923ddb55e3c99441307
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-12-16 15:20:38 +0100

    @Push eingefügt.

--- src/src/main/java/de/ostfalia/application/Application.java
+++ src/src/main/java/de/ostfalia/application/Application.java
@@ -1,6 +1,7 @@
 package de.ostfalia.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
+@Push
 @Theme(value = "flowcrmtutorial")
 public class Application implements AppShellConfigurator {
 
